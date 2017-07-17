7f8a8ecd3cdf5a3d173c364aa2d9f26eb15fa894
Douglasproglima <douglasproglima@gmail.com>
2017-07-17 23:54:00 +0000

    2 - Configuração do arquivo de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Aula 36 (12:00 minutos) - criando o nome aqui vc terá acesso externo exemplo:
+  #user
+  #cliente
+  #deste modo conseguiria acessar na ulr do browser passando localhost:3000/api/cliente 
+  namespace :api, defaults:    {format: json }, 
+                  constraints: {subdomain: 'api'},
+                  path: "/" do
+
+    
+  end
 end
