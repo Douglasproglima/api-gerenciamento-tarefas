1f13d43df3fa39a6d0cc3deab5ba20a97b4ec084
Douglasproglima <douglasproglima@gmail.com>
2017-07-18 00:16:42 +0000

    3.2 - COnfiguração do arquivo de rotas e subDominios

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #Aula 36 (12:00 minutos) - criando o nome aqui vc terá acesso externo exemplo:
+  #user
+  #cliente
+  #deste modo conseguiria acessar na ulr do browser passando localhost:3000/api/cliente 
+  namespace :api, defaults:    {format: json }, 
+                  constraints: {subdomain: 'api'},
+                  path: "/" do
+
+    #Acesso: api.nomeDoSite.com/nomeControllerDefinidoAqui
+    #Acesso Local: api.localhost:3000
+  end
 end
