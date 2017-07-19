da7e084205183afe5b0811851b218200016dccf8
Douglasproglima <douglasproglima@gmail.com>
2017-07-19 01:34:51 +0000

    4.5 - Teste com o shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { FactoryGirl.build(:user) }
-
+  # subject { FactoryGirl.build(:user) }
+  
   # before { @user = FactoryGirl.build(:user) }
 
   # it { expect(@user).to respond_to(:email) }
@@ -18,8 +18,38 @@ RSpec.describe User, type: :model do
   # it { expect(subject).to respond_to(:password_confirmation)}
   # it { expect(subject).to be_valid }
 
-  it { is_expected.to respond_to(:email)}
-  it { is_expected.to respond_to(:password)}
-  it { is_expected.to respond_to(:password_confirmation)}
-  it { is_expected.to be_valid }
+  # it { is_expected.to respond_to(:email)}
+  # it { is_expected.to respond_to(:password)}
+  # it { is_expected.to respond_to(:password_confirmation)}
+  # it { is_expected.to be_valid }
+
+  # it { expect(user).to respond_to(:password) }
+  # it { expect(user).to respond_to(:password_confirmation) }
+  # it { expect(user).to be_valid }
+
+  let(:user){ FactoryGirl.build(:user) }
+  #it { expect(user).to respond_to(:email) }
+
+  #Teste quando o usuário for vázio
+  # context 'when name is blank' do
+  #   before { user.name = " " }
+
+  #   it { expect(user).not_to be_valid }
+  # end
+
+  #Teste quando o usuário for null
+  # context 'when name is nill' do
+  #   before { user.name = " " }
+
+  #   it { expect(user).not_to be_valid }
+  # end
+
+############ Como usar o shoulda-matchers ############
+  #it { expect(user).to validate_presence_of(:name) }
+  # it {is_expected.to validate_numericality_of(:age) }
+  
+  it {is_expected.to validate_presence_of(:email) }
+  it {is_expected.to validate_uniqueness_of(:email).case_insensitive } #Testa se o e-mail é unico e case insensitive
+  it {is_expected.to validate_confirmation_of(:password) } #Testa se a senha é igual ao informado no password_confirmation
+  it {is_expected.to allow_value("douglasproglima@gmail.com").for(:email) }
 end
