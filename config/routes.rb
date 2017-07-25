42fc3e029089896f193126877d956966b1a603f7
Douglasproglima <douglasproglima@gmail.com>
2017-07-25 00:35:46 +0000

    4.13 - Adicionado a Action Update no cadastro de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     #Acesso Local: api.localhost:3000
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update]
     end
   end
 
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Users API', type: :request do
 
   before { host! 'api.taskmanager.dev' }
 
+  # Lista usuário passando o id
   describe 'GET /users/:id' do
     before do
       headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
@@ -33,6 +34,7 @@ RSpec.describe 'Users API', type: :request do
 
   end
 
+  # Inseri usuário
   describe 'POST /users' do
     #Antes de cada 'it' será executando a instrução dentro do 'before do ... end'
     before do
@@ -40,20 +42,20 @@ RSpec.describe 'Users API', type: :request do
       post '/users', params: { rest_user: user_params }, headers: headers 
     end
 
-    context 'Quando os parâmetros da requisição são válidos' do
+    context 'I - Quando os parâmetros da requisição são válidos' do
       let(:user_params) { FactoryGirl.attributes_for(:user) }
 
-      it 'Retorno da requisição código 201' do
+      it 'I - Retorno da requisição código 201' do
         expect(response).to  have_http_status(201)
       end
 
-      it 'Retorno do json data para o usuário criado' do
+      it 'I - Retorno do json data para o usuário criado' do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:email]).to eq(user_params[:email])
       end
     end
 
-    context 'Quando os parâmetros da requisição são inválidos' do
+    context 'I - Quando os parâmetros da requisição são inválidos' do
       let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
 
       it 'Retorno do status código 422' do
@@ -61,7 +63,41 @@ RSpec.describe 'Users API', type: :request do
         expect(response).to have_http_status(422)
       end
 
-      it 'Retorno do json data para o erro' do
+      it 'I - Retorno do json data para o erro' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
+
+  # Atualizar usuário
+  describe 'PUT /users/:id' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      put "/users/#{user_id}", params: {rest_user: user_params }, headers: headers 
+    end
+    
+    context 'U - Quando os parâmetros da requisição são válidos' do
+      let(:user_params) { { email: 'new_email_malaca@gmail.com.br' } }
+      
+      it 'U - Retorno do status de código 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'U - Retorno do json com os dados do usuário atualizado' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+
+    context 'U - Quando os parâmetros da requisição são inválidos' do
+      let(:user_params) { { email: 'new_email_invalido_malaca@' } }
+      
+      it 'U - Retorno do status de código 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'U - Retorno do json com os dados do usuário caso retorne erro' do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
